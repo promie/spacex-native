b4e690a0a2b69198f4c84355503b46cd72be0dc5
Promie Yutasane <promie.yutasane@gmail.com>
2024-02-19 13:29:58 +1100

    SPACE-07 render the images and list on the page

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ import {
 import { Link } from 'expo-router'
 import { useQuery } from '@tanstack/react-query'
 import { FlashList, ListRenderItem } from '@shopify/flash-list'
+import { Ionicons } from '@expo/vector-icons'
 import { getLaunches } from '@api/launches'
 
 const Page = () => {
@@ -21,14 +22,29 @@ const Page = () => {
   const { data, isLoading } = query
 
   const renderItem: ListRenderItem<any> = ({ item, index }) => (
-    <Link key={index} href={`/(launch)/${item.id}`} asChild>
-      <TouchableOpacity></TouchableOpacity>
+    <Link key={index} href={`/(launch)/${item.flight_number}`} asChild>
+      <TouchableOpacity>
+        <View style={styles.item}>
+          <Image
+            source={{ uri: item.links.patch.small }}
+            style={styles.preview}
+          />
+          <Text style={styles.itemText}>{item.name}</Text>
+          <Ionicons name="chevron-forward" size={24} color="black" />
+        </View>
+      </TouchableOpacity>
     </Link>
   )
 
   return (
     <View style={styles.container}>
       {isLoading && <ActivityIndicator style={styles.loadingIndication} />}
+      <FlashList
+        data={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <View />}
+        estimatedItemSize={100}
+      />
     </View>
   )
 }
@@ -38,7 +54,23 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   loadingIndication: {
-    minHeight: '100%',
+    marginTop: 15,
+  },
+  item: {
+    padding: 10,
+    height: 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  itemText: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+    flex: 1,
+  },
+  preview: {
+    width: 100,
+    height: 100,
   },
 })
 
