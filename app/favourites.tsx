569fdac2a992b11196be345f86c3b7fa082258d1
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-03 07:16:36 +1100

    SPACE-13 added a new favourites modal page

---
+++ app/favourites.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Page = () => {
+  return (
+    <View>
+      <Text>Favourites</Text>
+    </View>
+  )
+}
+export default Page
