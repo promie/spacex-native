07845cfeee6be010312470bf58c5fa531db9ea7b
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-02 15:53:57 +1100

    SPACE-12 added functionality to favourite individual flight

--- app/(flight)/[id].tsx
+++ app/(flight)/[id].tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { useLocalSearchParams, useNavigation } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useQuery } from '@tanstack/react-query'
+import { Ionicons } from '@expo/vector-icons'
 import { getFlightDetails } from '@api/launches'
 import LoadingIndicator from '@components/LoadingIndicator'
 
 const Page = () => {
+  const [isFavourite, setIsFavourite] = useState<boolean>(false)
   const { id } = useLocalSearchParams<{ id: string }>()
 
   const navigation = useNavigation()
@@ -18,6 +21,15 @@ const Page = () => {
   const { isLoading, data, isError } = flightDetailsQuery
 
   useEffect(() => {
+    const checkFavourite = async () => {
+      const favourite = await AsyncStorage.getItem(`favourite-${id}`)
+      setIsFavourite(favourite === 'true')
+    }
+
+    checkFavourite()
+  }, [])
+
+  useEffect(() => {
     if (!isError && data) {
       const title = data.mission_name
 
@@ -27,6 +39,27 @@ const Page = () => {
     }
   }, [data, isError])
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Text onPress={toggleFavourite}>
+          <Ionicons
+            name={isFavourite ? 'star' : 'star-outline'}
+            size={22}
+            color="#FFF"
+          />
+        </Text>
+      ),
+    })
+  }, [isFavourite])
+
+  const toggleFavourite = async () => {
+    await AsyncStorage.setItem(
+      `favourite-${id}`,
+      isFavourite ? 'false' : 'true',
+    )
+    setIsFavourite(!isFavourite)
+  }
   return (
     <View>
       {isLoading ? (
